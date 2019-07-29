a8cc958902007eedfb8bdc8b569b129810260035
Dengjianping <djptux@gmail.com>
2019-07-29 17:12:24 +0800

    refactored comments apis on db opearation

--- src/models/comment.rs
+++ src/models/comment.rs
@@ -1,34 +1,42 @@
-use chrono::prelude::*;
-use crate::models::{ schema::comments, post::Post };
-use crate::utils::utils::DBPool;
-use diesel::{ prelude::*, pg::PgConnection, result::Error as DBError, associations::Identifiable };
+use actix_web::web::Data;
+use chrono::{ Utc, NaiveDateTime, NaiveDate, Datelike }; // Datelike for month(), year(), day()
+use diesel::prelude::*;
 use serde_derive::{ Deserialize, Serialize };
 
+use crate::utils::utils::PgPool;
+use super::{ schema::{ self, comments }, post::Post };
 
 #[derive(Queryable, Serialize, Deserialize, AsChangeset, Debug, Identifiable, Associations)]
 #[table_name="comments"]
 #[belongs_to(Post)] // must derive Associations
-pub struct Comment {
-    pub id: i32,
-    pub username: String,
-    pub email: String,
-    pub comment: String,
-    pub committed_time: Option<NaiveDateTime>,
-    pub post_id: i32,
+pub(crate) struct Comment {
+    pub(crate) id: i32,
+    pub(crate) username: String,
+    pub(crate) email: String,
+    pub(crate) comment: String,
+    pub(crate) committed_time: Option<NaiveDateTime>,
+    pub(crate) post_id: i32,
 }
 
 #[derive(Insertable, Queryable, Serialize, Deserialize, AsChangeset, Debug)]
 #[table_name="comments"]
-pub struct NewComment {
-    pub username: String,
-    pub email: String,
-    pub comment: String,
-    pub committed_time: Option<NaiveDateTime>,
-    pub post_id: i32,
+pub(crate) struct NewComment {
+    pub(crate) username: String,
+    pub(crate) email: String,
+    pub(crate) comment: String,
+    pub(crate) committed_time: Option<NaiveDateTime>,
+    pub(crate) post_id: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub(crate) struct CreateComment {
+    pub(crate) comment: String,
+    pub(crate) username: String,
+    pub(crate) email: String,
 }
 
 impl NewComment {
-    pub fn new(comment: &CreateComment, article_id: i32) -> NewComment {
+    pub(crate) fn new(comment: &CreateComment, article_id: i32) -> Self {
         NewComment {
             username: comment.username.clone(),
             email: comment.email.clone(),
@@ -39,73 +47,50 @@ impl NewComment {
     }
 }
 
+pub(crate) struct CommentOperation;
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct CreateComment {
-    pub comment: String,
-    pub username: String,
-    pub email: String,
-}
-
-
-pub enum CommentHandle {
-    AllComment,
-    TodayComments, // how many new comments added
-    AllCommentByPost(i32),
-    InsertComment(NewComment),
-}
-
-
-// 'b: 'a means 'b lives as long as 'a at least, that I need.
-// impl<'a> From<actix_web::Form<CreateComment>> for NewComment<'a> {
-impl From<actix_web::Form<CreateComment>> for NewComment {
-    fn from(new_comment: actix_web::Form<CreateComment>) -> NewComment {
-        NewComment{
-            comment: new_comment.comment.clone(),
-            committed_time: Some(Utc::now().naive_utc()),
-            username: new_comment.username.clone(),
-            email: new_comment.email.clone(),
-            post_id: 1,
-        }
+impl CommentOperation {
+    pub(crate) fn get_all_comments(pool: &Data<PgPool>) -> Result<Vec<Comment>, failure::Error> {
+        use super::schema::comments::dsl::*;
+        let conn = &*pool.get()?;
+    
+        let all_comments = comments.order(schema::comments::id.desc()).load::<Comment>(conn)?;
+        Ok(all_comments)
     }
-}
 
-
-impl actix::Message for CommentHandle {
-    type Result = Result<Vec<Comment>, DBError>;
-}
-
-impl actix::Handler<CommentHandle> for DBPool {
-    type Result = Result<Vec<Comment>, DBError>;
-
-    fn handle(&mut self, msg: CommentHandle, _: &mut Self::Context) -> Self::Result {
-        use crate::models::schema::comments::dsl::*; // comments imported
-        use crate::models::schema;
-        // to use now fucntion, and IntervalDsl lets int and f64 can construct intervals.
-        // http://docs.diesel.rs/diesel/pg/expression/extensions/trait.IntervalDsl.html
-        // use diesel::dsl::{ now, IntervalDsl };
-        // use diesel::pg::data_types::PgInterval;
-
-        let conn: &PgConnection = &self.conn;
-        let comment_items = match msg {
-            CommentHandle::AllComment => {
-                comments.order(schema::comments::id.desc()).load::<Comment>(conn).expect("failed to load all comments.")
-            }
-            CommentHandle::TodayComments => {
-                let now_is: NaiveDateTime = Utc::now().naive_utc();
-                let today_begin: NaiveDateTime = NaiveDate::from_ymd(now_is.year(), now_is.month(), now_is.day()).and_hms(0, 0, 0);
-                let today_end: NaiveDateTime = NaiveDate::from_ymd(now_is.year(), now_is.month(), now_is.day() + 1).and_hms(0, 0, 0);
-                comments.filter(schema::comments::committed_time.between(today_begin, today_end)).load::<Comment>(conn).expect("failed to load related comments.")
-                // comments.filter(schema::comments::committed_time.gt(now - 1.0_f64.days())).load::<Comment>(conn).expect("failed to load related comments.")
-            }
-            CommentHandle::AllCommentByPost(comment_id) => {
-                comments.filter(schema::comments::post_id.eq(&comment_id)).load::<Comment>(conn).expect("failed to load related comments.")
-            }
-            CommentHandle::InsertComment(new_comment) => {
-                diesel::insert_into(comments).values(&new_comment).execute(conn).expect("failed to insert a new comment");
-                comments.filter(schema::comments::username.eq(&new_comment.username)).load::<Comment>(conn).expect("failed to load a comment.")
-            }
-        };
-        Ok(comment_items)
+    pub(crate) fn get_comments_by_post(comment_id: i32, pool: &Data<PgPool>) -> Result<Vec<Comment>, failure::Error> {
+        use super::schema::comments::dsl::*;
+        let conn = &*pool.get()?;
+        
+        let all_comments = comments.filter(schema::comments::post_id.eq(&comment_id)).load::<Comment>(conn)?;
+        Ok(all_comments)
+    }
+    
+    pub(crate) fn insert_comment(new_comment: NewComment, pool: &Data<PgPool>) -> Result<(), failure::Error> {
+        use super::schema::comments::dsl::*;
+        let conn = &*pool.get()?;
+        diesel::insert_into(comments).values(&new_comment).execute(conn)?;
+        Ok(())
+    }
+    
+    pub(crate) fn get_today_comments(pool: &Data<PgPool>) -> Result<Vec<Comment>, failure::Error> {
+        use super::schema::comments::dsl::*;
+        let conn = &*pool.get()?;
+        
+        let now_is: NaiveDateTime = Utc::now().naive_utc();
+        let today_begin: NaiveDateTime = NaiveDate::from_ymd(now_is.year(), now_is.month(), now_is.day()).and_hms(0, 0, 0);
+        let today_end: NaiveDateTime = NaiveDate::from_ymd(now_is.year(), now_is.month(), now_is.day() + 1).and_hms(0, 0, 0);
+        let today_comments = comments.filter(schema::comments::committed_time.between(today_begin, today_end)).load::<Comment>(conn)?;
+        Ok(today_comments)
+    }
+    
+    pub(crate) fn get_posts_by_comments(ids: impl AsRef<[i32]>, pool: &Data<PgPool>) -> Result<Vec<Post>, failure::Error> {
+        let conn = &*pool.get()?;
+        
+        // like [1, 2, 3, 4] => 1, 2, 3, 4
+        let rm_brackets: String = format!("{:?}", ids.as_ref()).chars().filter(|c| c.ne(&'[') && c.ne(&']')).collect();
+        let raw_sql = format!("SELECT * FROM posts WHERE id in ({})", rm_brackets);
+        let posts = diesel::sql_query(raw_sql).load::<Post>(conn)?;
+        Ok(posts)
     }
 }
