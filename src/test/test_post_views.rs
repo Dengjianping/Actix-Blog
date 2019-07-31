7c6981f77d8f2fbcfc0f036c14ab1e57f6632723
Dengjianping <djptux@gmail.com>
2019-07-31 23:30:40 +0800

    update test cases about views of post apis

--- src/test/test_post_views.rs
+++ src/test/test_post_views.rs
@@ -2,21 +2,19 @@
 
 use actix_web::{ test, web, App, http::header, http };
 use actix_files as fs;
-use actix_http::cookie::Cookie;
 use actix_session::CookieSession;
 use actix_service::Service;
 use bytes::Bytes;
-use log::{ info, debug };
-use serde::{Serialize, Deserialize};
+use serde::{ Serialize, Deserialize };
 
 use crate::views;
 use crate::utils::utils::db_pool;
 use crate::models::comment::CreateComment;
+use crate::models::contact::CreateContact;
 
-use super::service_on;
 
 #[test]
-fn test_index_get() {
+fn test_index() {
     let mut app = test::init_service(App::new().data(db_pool().unwrap().clone())
         .service(fs::Files::new("/static", "static/").show_files_listing())
         .service(
@@ -26,21 +24,23 @@ fn test_index_get() {
     
     let req = test::TestRequest::get().uri("/").to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), http::StatusCode::OK);
 }
 
 #[test]
-fn test_index_post() {
-    let mut app = test::init_service(App::new().data(db_pool().unwrap().clone())
+#[ignore]
+#[should_panic(expected = "this page is unimplemented.")]
+fn test_page_404() {
+    let mut app = test::init_service(App::new()
         .service(fs::Files::new("/static", "static/").show_files_listing())
         .service(
-            web::scope("/").service(web::resource("").route(web::get().to_async(views::post::show_all_posts)))
+            web::scope("/").service(web::resource("/not_found/").route(web::get().to_async(views::post::page_404)))
         )
     );
     
-    let req = test::TestRequest::post().uri("/").to_request();
+    let req = test::TestRequest::get().uri("/not_found/").to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_client_error());
+    assert_eq!(resp.status(), http::StatusCode::OK);
 }
 
 #[test]
@@ -54,7 +54,7 @@ fn test_about() {
     
     let req = test::TestRequest::get().uri("/about/").to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), http::StatusCode::OK);
 }
 
 #[test]
@@ -68,7 +68,7 @@ fn test_contact() {
     
     let req = test::TestRequest::get().uri("/contact/").to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), http::StatusCode::OK);
 }
 
 #[test]
@@ -82,7 +82,7 @@ fn test_all_post() {
     
     let req = test::TestRequest::get().uri("/all_posts/").to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), http::StatusCode::OK);
 }
 
 #[test]
@@ -97,11 +97,10 @@ fn test_all_pagination() {
     // let req = test::TestRequest::get().uri("/page/{page_num}/").param("page_num", "2").to_request();
     let req = test::TestRequest::get().uri("/page/1/").to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), http::StatusCode::OK);
 }
 
 #[test]
-// #[should_panic(expected = "page not found")]
 fn test_all_pagination_failure() {
     let mut app = test::init_service(App::new().data(db_pool().unwrap().clone())
         .service(fs::Files::new("/static", "static/").show_files_listing())
@@ -113,41 +112,90 @@ fn test_all_pagination_failure() {
     // let req = test::TestRequest::get().uri("/page/{page_num}/").param("page_num", "2").to_request();
     let req = test::TestRequest::get().uri("/page/100/").to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), http::StatusCode::OK);
+}
+
+#[test]
+fn test_add_contact() {
+    let mut app = test::init_service(App::new().data(db_pool().unwrap().clone())
+        .service(fs::Files::new("/static", "static/").show_files_listing())
+        .service(
+            web::scope("/").service(web::resource("/add_contact/").route(web::post().to_async(views::post::add_contact)))
+        )
+    );
+    
+    let new_contact = CreateContact {
+        tourist_name: "jamie".to_owned(),
+        email: "example.bob@actix.com".to_owned(),
+        message: "I like you content".to_owned(),
+    };
+    let req = test::TestRequest::post().uri("/add_contact/")
+                                       .header(header::CONTENT_TYPE, "application/json")
+                                       .set_json(&new_contact)
+                                       .to_request();
+    let result: bool = test::read_response_json(&mut app, req);
+    assert_eq!(result, true);
 }
 
 #[test]
 fn test_add_comment() {
     let mut app = test::init_service(App::new().data(db_pool().unwrap().clone())
-        .wrap(CookieSession::signed(&[0; 32]).secure(false))
+        .wrap(CookieSession::signed(&[0; 32]).name("post_session").secure(false))
         .service(fs::Files::new("/static", "static/").show_files_listing())
         .service(
-            web::scope("/").service(web::resource("/add_comment/").route(web::post().to_async(views::post::add_comment)))
+            web::scope("/").service(web::resource("/article/{title}/").route(web::get().to_async(views::post::post_detail)))
+                           .service(web::resource("/add_comment/").route(web::post().to_async(views::post::add_comment)))
         )
     );
+    
+    // set session
+    let req = test::TestRequest::get().uri("/article/Cow%20in%20Rust/").to_request();
+    let resp = test::block_on(app.call(req)).unwrap();
+    
+    let cookie = resp.response().cookies().find(|c| c.name() == "post_session").clone();
+    assert!(cookie.is_some());
     
     let comment = CreateComment {
         comment: "good post!".to_owned(),
         username: "Bob".to_owned(),
         email: "djptux@gmail.com".to_owned(),
     };
-    let cookie = Cookie::new("article_id", "1");
-    
     let req = test::TestRequest::post().uri("/add_comment/")
                                        .header(header::CONTENT_TYPE, "application/json")
                                        .set_json(&comment)
-                                       .cookie(cookie)
+                                       .cookie(cookie.unwrap())
                                        .to_request();
     let result: bool = test::read_response_json(&mut app, req);
     assert_eq!(result, true);
-    // let result = test::read_response(&mut app, req);
-    // let resp = test::call_service(&mut app, req);
-    // let result = test::read_body(resp);
-    // let resp = test::block_on(app.call(req)).unwrap();
-    // assert!(resp.status().is_success());
-    // assert_eq!(resp.status().is_success(), true);
-    // assert_eq!(resp.status(), http::StatusCode::OK);
-    // assert_eq!(resp, Bytes::from_static(b"true"));
+}
+
+#[test]
+fn test_user_likes() {
+    let mut app = test::init_service(App::new().data(db_pool().unwrap().clone())
+        .wrap(CookieSession::signed(&[0; 32]).name("post_session").secure(false))
+        .service(fs::Files::new("/static", "static/").show_files_listing())
+        .service(
+            web::scope("/").service(web::resource("/article/{title}/").route(web::get().to_async(views::post::post_detail)))
+                           .service(web::resource("/user_likes/").route(web::post().to_async(views::post::user_likes)))
+        )
+    );
+    
+    // set session
+    let req = test::TestRequest::get().uri("/article/Cow%20in%20Rust/").to_request();
+    let resp = test::block_on(app.call(req)).unwrap();
+    
+    let cookie = resp.response().cookies().find(|c| c.name() == "post_session").clone();
+    assert!(cookie.is_some());
+    
+    new_struct!(Like, pub, [Debug, Clone, Serialize, Deserialize], (likes_count=>i32));
+    let likes = Like { likes_count: 6 };
+    let req = test::TestRequest::post().uri("/user_likes/")
+                                       .header(header::CONTENT_TYPE, "application/json")
+                                       .set_json(&likes)
+                                       .cookie(cookie.unwrap())
+                                       .to_request();
+    let result: bool = test::read_response_json(&mut app, req);
+    assert_eq!(result, true);
 }
 
 #[test]
@@ -160,11 +208,9 @@ fn test_post_detail() {
         )
     );
     
-    // let cookie = Cookie::new("article_id", "1");
-    //let req = test::TestRequest::get().uri("/article/Cow%20in%20Rust/").to_request();
-    let req = test::TestRequest::get().uri("/article/").param("title", "Cow in Rust").to_request();
+    let req = test::TestRequest::get().uri("/article/Cow%20in%20Rust/").to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), http::StatusCode::OK);
 }
 
 #[test]
@@ -177,8 +223,19 @@ fn test_search() {
     );
     
     new_struct!(Search, pub, [Debug, Clone, Serialize, Deserialize], (key_word=>String));
-    let search = Search { key_word: "python".to_owned() };
-    let req = test::TestRequest::post().uri("/search/").set_json(&search).to_request();
+    let req = test::TestRequest::post()
+                .uri("/search/")
+                .header(header::CONTENT_LENGTH, "13")
+                .header(header::CONTENT_TYPE, "application/x-www-form-urlencoded")
+                .set_payload(Bytes::from_static(b"key_word=rust"))
+                .to_request();
+    
+    /*
+    let resp = test::call_service(&mut app, req);
+    let result = test::read_body(resp);
+    assert_eq!(result, Bytes::from_static(b"welcome!"));
+    */
+    
     let resp = test::block_on(app.call(req)).unwrap();
-    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), http::StatusCode::OK);
 }
