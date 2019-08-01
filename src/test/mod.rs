e6828bd5c281605c8da08ccf85cce482395b089a
Dengjianping <djptux@gmail.com>
2019-08-01 23:02:17 +0800

    add a function to generate random string

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,28 +1,12 @@
 pub(self) mod test_auth_views;
 pub(self) mod test_post_views;
 
-use actix_web::dev::Service;
-use actix_web::{test, web, App};
-use actix_files as fs;
+use std::iter;
+use rand::{Rng, thread_rng};
+use rand::distributions::Alphanumeric;
 
-use crate::views;
-use crate::utils::utils::db_pool;
-use lazy_static::lazy_static;
-
-/*
-lazy_static! {
-    pub(crate) static ref A: impl Service = test::init_service(
-        App::new()
-            .service(web::resource("/test").to(|| HttpResponse::Ok()))
-    );
-}
-*/
-
-pub(self) fn service_on() -> impl Service {
-    test::init_service(App::new().data(db_pool().unwrap().clone())
-        .service(fs::Files::new("/static", "static/").show_files_listing())
-        .service(
-            web::scope("/").service(web::resource("").route(web::get().to_async(views::post::show_all_posts)))
-        )
-    )
+pub(self) fn generate_random_string(length: usize) -> String {
+    iter::repeat(()).map(|()| thread_rng().sample(Alphanumeric))
+                    .take(length)
+                    .collect::<String>()
 }
